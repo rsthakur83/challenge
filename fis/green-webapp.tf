915b293c08d04c903166ce9873c07430fb1adcf0
rsthakur83 <rsthakur83@yahoo.com>
2017-07-01 20:37:20 +0700

    Update green-webapp.tf

--- fis/green-webapp.tf
+++ fis/green-webapp.tf
@@ -4,13 +4,32 @@ provider "aws" {
 
 
 
-
-
 resource "aws_launch_configuration" "machine-factory-v2" {
     name = "machine-factory-v2"
     image_id = "ami-b63769a1"
-     security_groups = ["sg-e9c77498","sg-11ce7d60"]
+    security_groups = ["sg-7056ec01","sg-7655ef07"]
     instance_type = "t2.micro"
+    key_name = "myappkeypair"
     user_data       = "${file("userdata.sh")}"
     lifecycle              { create_before_destroy = true }
+}
+
+
+resource "aws_autoscaling_group" "machine-factory-v2" {
+  availability_zones = ["us-east-1b", "us-east-1c"]
+  name = "machine-factory-v2"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 2
+  health_check_grace_period = 80
+  health_check_type = "ELB"
+  force_delete = false
+  launch_configuration = "${aws_launch_configuration.machine-factory-v2.name}"
+  load_balancers = ["web-elb"]
+  vpc_zone_identifier = ["subnet-2dba3f01","subnet-d2334e9a"]
+}
+
+
+output "Autoscaling group" {
+  value = "${aws_autoscaling_group.machine-factory-v2.name}"
 }
